a8f33de313f33021c9d45eca9518ca9f00cc8eb1
razec2112 <razec.cz@gmail.com>
2023-04-18 21:28:57 -0400

    clase de Object

---
+++ main.js
@@ -0,0 +1,47 @@
+const juan = {
+    name: "Juanito",
+    age: 18,
+    approvedCourses: ["Curso 1"],
+    addCourses(newCourse) {
+        console.log("this", this)
+        console.log("this.approvedCourses",this.approvedCourses)
+        this.approvedCourses.push(newCourse);
+    }
+};
+
+// console.log(Object.keys(juan))
+// console.log(Object.getOwnPropertyNames(juan))
+// console.log(Object.entries(juan))
+
+
+// Object.defineProperty(juan, "navigator", {
+//     value: "Chrome",
+//     enumerable: false,// hace que no se pueda enumerar en un array cuando se usa el Object.keys
+//     writable: true,
+//     configurable: true,
+// }) 
+
+// Object.defineProperty(juan, "editor", {
+//     value: "VSCode",
+//     enumerable: true,
+//     writable: false,// evita que el valor de la propiedad se cambie, pero si se puede eliminar
+//     configurable: true,
+// }) 
+
+// Object.defineProperty(juan, "terminal", {
+//     value: "WSL",
+//     enumerable: true,
+//     writable: true,
+//     configurable: false,// evita que la propiedad se pueda borrar
+// }) 
+
+// Object.defineProperty(juan, "pruabaNASA", {
+//     value: "Extraterrestre",
+//     enumerable: false,
+//     writable: false,
+//     configurable: false,
+// }) 
+
+Object.seal(juan)
+
+console.log(Object.getOwnPropertyDescriptors(juan))
